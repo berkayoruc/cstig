5a9391b9fbd41ef5db5b987cc3f95a513b99b961
Berkay Oruç <berkayoruc@hotmail.com>
2018-10-15 15:06:53 +0300

    Update ellipsoidalCart2CIOCart.py

--- ellipsoidalCart2CIOCart.py
+++ ellipsoidalCart2CIOCart.py
@@ -15,4 +15,4 @@ def ellipsoidalCart2CIOCart(Xp,Yp,Zp,X0,Y0,Z0,ex,ey,ez,f0):
     print("Y : ", Y, " m")
     print("Z : ", Z, " m")
 
-ellipsoidalCart2CIOCart(3987577.612,2779005.035,4117452.563,135.768,81.345,113.928,-2.3,4.8,5.6,0.00000175)
+#ellipsoidalCart2CIOCart(3987577.612,2779005.035,4117452.563,135.768,81.345,113.928,-2.3,4.8,5.6,0.00000175)
